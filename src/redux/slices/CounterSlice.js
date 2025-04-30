c61aba6bda239b1f98ca71ae173f894535f25932
devrathrana <devrath.rana@bold.com>
2025-04-30 09:47:07 +0530

    update rtk methods description

--- src/redux/slices/CounterSlice.js
+++ src/redux/slices/CounterSlice.js
@@ -14,7 +14,23 @@ const counterSlice = createSlice({
 });
 
 
+/*
+This defines a memoized selector getCount to extract the counter slice from the Redux store.
+
+It assumes that in your Redux store, the reducer for this slice is mounted at state.counter.
+
+It just returns the value as-is (no transformation).
+
+createSelector creates a memoized selector — a function that returns derived or specific pieces of the Redux state, and only re-computes if its input changes.
+
+inputSelector: A function that selects a slice of the state (e.g., state.counter)
+
+resultFunc: A function that receives the result of the inputSelector and returns the final value (optional)
+*/
 export const getCount = createSelector(store => store.counter, counterSlice => counterSlice)
 
+//action creators
 export const { decrement, increment } = counterSlice.actions
+
+//Exports the reducer function (so it can be added to the Redux store)
 export default counterSlice.reducer;
